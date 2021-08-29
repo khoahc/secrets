369c80eee3f4584f537ae3fda51e48fa51829fe2
Khoa <khoahc98@gmail.com>
2021-08-29 12:00:14 -0400

    Level 2 - Encryption

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const port = process.env.PORT || 3000;
 
@@ -14,6 +14,24 @@ app.use(
   })
 );
 
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+var secret = "thisissecret";
+userSchema.plugin(encrypt, {
+  secret: secret,
+  encryptedFields: ["password"],
+});
+
+const User = new mongoose.model("User", userSchema);
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -24,6 +42,40 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.get("/secrets", (req, res) => {
+  res.render("secrets");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  user.save((err) => {
+    if (!err) {
+      res.render("secrets");
+      console.log("Registered successfully");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ email: username }, (err, foundUser) => {
+    if (!err) {
+      if (foundUser.password === password) {
+        res.render("secrets");
+        console.log("Login successfully");
+      }
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 app.listen(port, () => {
