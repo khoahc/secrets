6e60fcab15cfbdc61a69ff85d449ebbf261d031b
Khoa <khoahc98@gmail.com>
2021-08-29 04:21:27 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.listen(port, () => {
+  console.log("server listening on port 3000");
+});
